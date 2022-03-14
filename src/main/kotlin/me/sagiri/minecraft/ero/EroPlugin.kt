fedb060b4714d36d663fb1d794a8026f0876e297
sagiri <i@sagiri.me>
2022-03-14 10:29:54 +0800

    qwq

--- src/main/kotlin/me/sagiri/minecraft/ero/EroPlugin.kt
+++ src/main/kotlin/me/sagiri/minecraft/ero/EroPlugin.kt
@@ -34,59 +34,16 @@ class EroPlugin : JavaPlugin()  {
 }
 
 class EroCommandExecutor : CommandExecutor {
-    lateinit var eroPlugin: EroPlugin
+    var eroPlugin: EroPlugin
 
     constructor(eroPlugin: EroPlugin) {
         this.eroPlugin = eroPlugin
     }
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if(sender is Player) {
-            val playerGetEroPicture = PlayerGetEroPictureEvent(sender, true)
-            Bukkit.getPluginManager().callEvent(playerGetEroPicture)
-        }
         EroScope.launch {
             val url = LoliApp.get(if (eroPlugin.config.getBoolean("r18")) 2 else 1)!!.data[0].url
             sender.sendMessage(url)
-            if(sender is Player) {
-                val playerGetEroPictureEvented = PlayerGetEroPictureEventEd(sender, true, url)
-                Bukkit.getPluginManager().callEvent(playerGetEroPictureEvented)
-            }
         }
         return true
-    }
-}
-
-class PlayerGetEroPictureEvent : PlayerEvent {
-    companion object {
-        private val HANDLERS = HandlerList()
-    }
-    override fun getHandlers(): HandlerList {
-        return HANDLERS
-    }
-
-    constructor(player : Player) : super(player) {
-
-    }
-
-    constructor(player: Player, async : Boolean) : super(player, async) {
-
-    }
-}
-
-class PlayerGetEroPictureEventEd : PlayerEvent {
-    lateinit var url: String
-    companion object {
-        private val HANDLERS = HandlerList()
-    }
-    override fun getHandlers(): HandlerList {
-        return HANDLERS
-    }
-
-    constructor(player : Player, url : String) : super(player) {
-        this.url = url
-    }
-
-    constructor(player: Player, async : Boolean, url: String) : super(player, async) {
-        this.url = url
     }
 }
